8542d44af89b1ca9b635022eeea17361644506a8
Darron Froese <darron@froese.org>
2014-04-18 07:55:30 -0600

    ChefSpec tests.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -5,7 +5,37 @@ require 'spec_helper'
 describe 'consul::default' do
   let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
-  it 'does something' do
-    pending 'Replace this with meaningful tests'
+  it 'installs the unzip package' do
+    expect(chef_run).to install_package('unzip')
   end
+
+  it 'creates the config path' do
+    expect(chef_run).to create_directory('/etc/consul')
+  end
+
+  it 'creates the config.d path' do
+    expect(chef_run).to create_directory('/etc/consul/config.d')
+  end
+
+  it 'creates the data_dir' do
+    expect(chef_run).to create_directory('/var/cache/consul')
+  end
+
+  it 'creates the init/consul.conf file' do
+    expect(chef_run).to create_template('/etc/init/consul.conf')
+  end
+
+  it 'creates the init.d/consul symlink' do
+    expect(chef_run).to create_link('/etc/init.d/consul')
+  end
+
+  it 'creates the config file' do
+    expect(chef_run).to create_template('/etc/consul/config.json')
+  end
+
+  it 'enables and starts the service' do
+    expect(chef_run).to enable_service('consul')
+    expect(chef_run).to start_service('consul')
+  end
+
 end
