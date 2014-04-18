b7e329a23ed6ba7212a16d97fdf943e9ca9ffa79
Darron Froese <darron@froese.org>
2014-04-18 07:55:49 -0600

    ServerSpec tests.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,40 @@ require 'spec_helper'
 
 # Write integration tests with Serverspec - http://serverspec.org/
 describe 'consul::default' do
-  it 'does something' do
-    pending 'Replace this with meaningful tests'
+  describe package('unzip') do
+    it { should be_installed }
+  end
+
+  describe file('/etc/consul') do
+    it { should be_directory }
+  end
+
+  describe file('/etc/consul/config.d') do
+    it { should be_directory }
+  end
+
+  describe file('/var/cache/consul') do
+    it { should be_directory }
+  end
+
+  describe file('/etc/init/consul.conf') do
+    it { should be_file }
+  end
+
+  describe file('/etc/init.d/consul') do
+    it { should be_file }
+  end
+
+  describe file('/etc/consul/config.json') do
+    it { should be_file }
+  end
+
+  describe file('/usr/local/bin/consul') do
+    it { should be_file }
+  end
+
+  describe service('consul') do
+    it { should be_enabled }
+    it { should be_running }
   end
 end
